d3e8c8c5c3bfadcbf9ad6f1a011229c348afccfc
Abhi_Learning <kvvsmoc@gmail.com>
2024-09-07 06:28:08 -0500

    Update cart quantity & cart design

--- src/components/cart-item/cart-item.component.jsx
+++ src/components/cart-item/cart-item.component.jsx
@@ -4,8 +4,13 @@ const CartItem = ({ cartItem }) => {
   const { name, quantity, price, imageUrl } = cartItem;
   return (
     <div className="cart-item-container">
-      <h2>{name}</h2>
-      <span>{quantity}</span>
+      <img src={imageUrl} alt={`${name}`} />
+      <div className="item-details">
+        <span className="name">{name}</span>
+        <span className="price">
+          {quantity} x ${price}
+        </span>
+      </div>
     </div>
   );
 };
